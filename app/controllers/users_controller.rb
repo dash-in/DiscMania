91730f6b6acde83422774d248f034303f144887b
atsuto suganuma <86strikepackage@gmail.com>
2019-08-09 15:45:59 +0900

    [finish]users-routing

--- app/controllers/users_controller.rb
+++
@@ -1,8 +0,0 @@
-class UsersController < ApplicationController
-  def deleting
-  end
-
-  def deleted
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,4 @@ Rails.application.routes.draw do
   resources :admins
 
   resources :settings, only: [:show, :edit, :update]
-  resources :users, only: [:show, :edit, :update, :destroy]
-  get '/users/deleting' => 'users#deleting'
-  get '/users/deleted' => 'users#deleted'
 end
