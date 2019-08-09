2f2f91d576e0c57d08c3dcfc7e75998fc71f8475
atsuto suganuma <86strikepackage@gmail.com>
2019-08-09 15:20:00 +0900

    [finish]users routing

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,7 @@ Rails.application.routes.draw do
   resources :admins
 
   resources :settings, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update, :destroy]
+  get '/users/deleting' => 'users#deleting'
+  get '/users/deleted' => 'users#deleted'
 end
