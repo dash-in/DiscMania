1585274ecdadd03db383d01e3cce2e57816c4835
sbtn_msr <yhi.mnk.22@gmail.com>
2019-08-11 04:42:22 +0000

    edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,16 @@ class UsersController < ApplicationController
   def deleted
   end
 
+    def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to users_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :address, :post_number, :tel, :email, :password, :image, :handlename, :dl_flg)
