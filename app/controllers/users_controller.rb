2638cab83eb4026c0b8cf1e6fc750eec0b70c385
sbtn_msr <yhi.mnk.22@gmail.com>
2019-08-11 03:51:27 +0000

    user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,16 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to users_path
+  end
+
+private
+def user_params
+    params.require(:user).permit(:handlename,:first_name,:last_name,:first_name_kana,:last_name_kana,:post_number,:address,:tel,
+:email,:password)
+end
+
 end
