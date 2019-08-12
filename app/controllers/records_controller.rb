f1551c8888c6b9b9203b15d64ac70f4075468149
morijun <morijun0806@gmail.com>
2019-08-12 10:02:45 +0000

    add artist_id column to Records

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,24 +1,13 @@
 class RecordsController < ApplicationController
 
+    def top
+
+    end
+
     def index
     end
 
     def show
-    end
-    
-    def new
-    end
-
-    def create
-    end
-
-    def edit
-    end
-
-    def update
-    end
-
-    def destroy
     end
 
 end
