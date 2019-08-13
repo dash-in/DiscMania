f92c313a51089074d429d1e37962344e0b9444fd
atsuto suganuma <86strikepackage@gmail.com>
2019-08-13 16:21:07 +0900

    [fix]user#update

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -21,13 +21,13 @@ class Admin::UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-       redirect_to users_path
+       redirect_to admin_users_path
     else render :edit
     end
   end
 
   private
   def user_params
-    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :address, :post_number, :tel, :email, :password, :image, :handlename, :dl_flg)
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :address, :post_number, :tel, :email, :password, :image, :handlename)
   end
 end
