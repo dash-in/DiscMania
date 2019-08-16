7b285e6eb11708715ab7ed4824eae9dac1bb6384
morijun <morijun0806@gmail.com>
2019-08-16 12:19:20 +0000

    complete admin/records without show action

--- app/controllers/admin/records_controller.rb
+++ app/controllers/admin/records_controller.rb
@@ -1,17 +1,31 @@
 class Admin::RecordsController < ArtistsController
 
-  before_action :set_record, only: [:edit, :update]
+  before_action :set_record, only: [:show, :edit, :update, :destroy]
 
   def index
-    @q = Form::Record.search
+    @q = Form::Record.includes(:tune).search
+    @jpops = Form::Record.where(genre: "Japanese Rock / Pops / Indies").limit(4).order(:stock)
+    @pops = Form::Record.where(genre: "Rock / Pops / Indies").limit(4).order(:stock)
+    @edms = Form::Record.where(genre: "Electric Dance Music").limit(4).order(:stock)
+    @ors = Form::Record.where(genre: "Old Rock").limit(4).order(:stock)
+    @hrs = Form::Record.where(genre: "Hard Rock / Heavy Metal").limit(4).order(:stock)
+    @punks = Form::Record.where(genre: "PUNK").limit(4).order(:stock)
+    @jazzs = Form::Record.where(genre: "JAZZ").limit(4).order(:stock)
+    @classics = Form::Record.where(genre: "CLASSIC").limit(4).order(:stock)
+    @sts = Form::Record.where(genre: "Sound Track").limit(4).order(:stock)
+    @sks = Form::Record.where(genre: "昭和歌謡曲 / 演歌").limit(4).order(:stock)
+    @idles = Form::Record.where(genre: "アイドル").limit(4).order(:stock)
+    @kpops = Form::Record.where(genre: "K-POP").limit(4).order(:stock)
+    @setting = Setting.find(1)
   end
 
   def search
-    @q = Form::Record.search(search_params)
-    @records = @q
-      .result
-      .order(:artist_name, created_at: :desc)
-      @setting = Setting.find(1)
+    # @q = Form::Record.search(search_params)
+    # @records = @q
+    #   .result(distinct: true)
+    #   .order(:artist_name, created_at: :desc)
+    @q = Form::Record.search(params[:q])
+    @records = @q.result(distinct: true).order(:stock)
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,55 @@ Setting.find_or_create_by(id: 1) do |setting|
     setting.shop_address = "長野県安曇野市xx-xx-xx"
     setting.shop_tel = "050-xxxx-xxxx"
 end
+
+Artist.find_or_create_by(id: 1) do |artist|
+  artist.name = "Mr.Children"
+  artist.description = "テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
+end
+
+Artist.find_or_create_by(id: 2) do |artist|
+  artist.name = "サザンオールスターズ"
+  artist.description = "テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
+end
+
+Artist.find_or_create_by(id: 3) do |artist|
+  artist.name = "米津玄師"
+  artist.description = "テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
+end
+
+Artist.find_or_create_by(id: 4) do |artist|
+  artist.name = "Mr.Big"
+  artist.description = "テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
+end
+
+Artist.find_or_create_by(id: 5) do |artist|
+  artist.name = "Guns'n Roses"
+  artist.description = "テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
+end
+
+Artist.find_or_create_by(id: 6) do |artist|
+  artist.name = "Bruno Mars"
+  artist.description = "テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
+end
+
+Artist.find_or_create_by(id: 7) do |artist|
+  artist.name = "Bon Jovi"
+  artist.description = "テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
+end
+
+Artist.find_or_create_by(id: 8) do |artist|
+  artist.name = "Bump of Chicken"
+  artist.description = "テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
+end
+
+Artist.find_or_create_by(id: 9) do |artist|
+  artist.name = "Man with a Mission"
+  artist.description = "テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
+end
+
+ShippingInfo.find_or_create_by(id: 1) do |shipping_info|
+  shipping_info.user_id = 1
+  shipping_info.name = "ユーザー１太郎"
+  shipping_info.post_number = "xxx-xxxx"
+  shipping_info.address = "東京都渋谷区渋谷xx-xx-xx"
+end
