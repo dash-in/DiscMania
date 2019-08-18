53625a8da8cf675c942b2cc8a6ca90bf18d7c510
atsuto suganuma <86strikepackage@gmail.com>
2019-08-18 11:53:09 +0900

    keep data

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,5 +2,6 @@ class CartItem < ApplicationRecord
 
   belongs_to :user
   belongs_to :record
-  
+  validates :quantity, presence: true
+  validates :quantity, numericality: { only_integer: true, greater_than: 0 }
 end
