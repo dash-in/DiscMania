1a3a086c3120088a94008be205f9ce0c2a424a4e
atsuto suganuma <86strikepackage@gmail.com>
2019-08-26 21:16:40 +0900

    [Add&Fix]kaminari

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,7 +7,9 @@ class Admin::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @shippings = @user.shipping_infos
+    @shippinga = ShippingInfo.page(params[:page]).per(5)
     @order_details = Order.where(user_id: params[:id]).includes(:order_details).map(&:order_details).flatten
+    @orderdetail = OrderDetail.page(params[:page]).per(5)
     @setting = Setting.find(1)
   end
 
